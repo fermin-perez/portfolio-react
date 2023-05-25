336e21e7a3b537dbba4f6c0b750bc64a00d836bb
Fermin Perez <fermin_05@hotmail.com>
2023-05-24 21:21:44 -0300

    Update Contact

--- src/components/resume/Education.jsx
+++ src/components/resume/Education.jsx
@@ -32,20 +32,19 @@ export const Education = () => {
             title='Wordpress'
             subTitle='Coderhouse (Marzo 2021 - Abril 2021)'
             result='10'
-            des='En este curso aprenderás a instalar y configurar WordPress utilizando un servidor apache local, que te permitirá tener dominio sobre el sitio web, además de usar los mejores complementos y herramientas para diseñar tu sitio completamente funcional y seguro. Crearás tu propia Tienda Online con pasarelas de pago. Al terminar el curso serás capaz de crear y configurar sitios web totalmente responsive, con e-commerce incluido.'
+            des='Crearás tu propia Tienda Online con pasarelas de pago. Al terminar el curso serás capaz de crear y configurar sitios web totalmente responsive, con e-commerce incluido.'
           />
           <ResumeCard
             title='JavaScript Moderno: Guía para dominar el lenguaje'
             subTitle='Udemy (Marzo 2022 - Marzo 2022)'
             result=''
-            des='El objetivo principal del curso es enseñarte JavaScript actual, empezando de cero conocimiento en JavaScript hasta llevarte a un nivel avanzado y competitivo en el mercado laboral actual. Este curso te ayudará a entrar fácilmente a frameworks basados en este lenguaje de programación y ayudarte a mejorar tus habilidades actuales en JavaScript.'
+            des='El objetivo principal del curso es enseñarte JavaScript actual, empezando de cero conocimiento en JavaScript hasta llevarte a un nivel avanzado y competitivo en el mercado laboral actual.'
           />
           <ResumeCard
             title='React Js'
             subTitle='Coderhouse (Julio 2022 - Agosto 2022)'
             result='8'
-            des='En este curso, que es el tercer nivel de la carrera de programación, aprenderás a programar por componentes, mediante Javascript, JS, ES6, y también conocerás las ventajas de la utilización del flujos de datos.
-            Asimismo, aplicarás el manejo de rutas utilizando Firebase, y comprenderás la utilización del virtual DOM mediante los desarrollos de React JS.
+            des='En este curso aprenderás a programar por componentes, mediante Javascript, JS, ES6, y también conocerás las ventajas de la utilización del flujos de datos.
             Al finalizar, podrás crear tus propias aplicaciones SPA, y estarás en condiciones de hacer cualquier desarrollo con uno de los librerias más populares y avanzadas del momento.'
           />
         </div>

540f9fd6cfb17def8b7e736c2bf28b7824e8a1f7
Fermin Perez <fermin_05@hotmail.com>
2023-09-28 11:02:13 -0300

    Add Node Education

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,18 +1,18 @@
-import { useRef, useState } from 'react';
-import { Title } from '../layouts/Title';
-import { ContactLeft } from './ContactLeft';
-import emailjs from '@emailjs/browser';
+import { useRef, useState } from "react";
+import { Title } from "../layouts/Title";
+import { ContactLeft } from "./ContactLeft";
+import emailjs from "@emailjs/browser";
 
 export const Contact = () => {
   const form = useRef();
 
-  const [name, setName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
-  const [errMsg, setErrMsg] = useState('');
-  const [successMsg, setSuccessMsg] = useState('');
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
 
   // ========== Email Validation start here ==============
   const emailValidation = () => {
@@ -26,26 +26,31 @@ export const Contact = () => {
     e.preventDefault();
 
     try {
-      if (name === '') {
-        setErrMsg('Apellido y Nombre son requeridos!');
-      } else if (phoneNumber === '') {
-        setErrMsg('Teléfono es requerido!');
-      } else if (email === '') {
-        setErrMsg('Email es requerido!');
+      if (name === "") {
+        setErrMsg("Apellido y Nombre son requeridos!");
+      } else if (phoneNumber === "") {
+        setErrMsg("Teléfono es requerido!");
+      } else if (email === "") {
+        setErrMsg("Email es requerido!");
       } else if (!emailValidation(email)) {
-        setErrMsg('Ingrese un email valido!');
-      } else if (subject === '') {
-        setErrMsg('Asunto es requerido!');
-      } else if (message === '') {
-        setErrMsg('Mensaje es requerido!');
+        setErrMsg("Ingrese un email valido!");
+      } else if (subject === "") {
+        setErrMsg("Asunto es requerido!");
+      } else if (message === "") {
+        setErrMsg("Mensaje es requerido!");
       } else {
-        await emailjs.sendForm('', '', form.current, '');
-        setErrMsg('');
-        setName('');
-        setPhoneNumber('');
-        setEmail('');
-        setSubject('');
-        setMessage('');
+        await emailjs.sendForm(
+          "service_rnza4dm",
+          "template_psd0a95",
+          form.current,
+          "cF1ecp9EEcNUCeflf"
+        );
+        setErrMsg("");
+        setName("");
+        setPhoneNumber("");
+        setEmail("");
+        setSubject("");
+        setMessage("");
         setSuccessMsg(`Gracias ${name}, ¡Tu mensaje se han enviado con éxito!`);
       }
     } catch (error) {
@@ -54,121 +59,121 @@ export const Contact = () => {
   };
   return (
     <section
-      id='contacto'
-      className='w-full py-20 border-b-[1px] border-b-black'
+      id="contacto"
+      className="w-full py-20 border-b-[1px] border-b-black"
     >
-      <div className='flex justify-center items-center text-center'>
-        <Title title='CONTACTO' des='Enviame tu consulta' />
+      <div className="flex justify-center items-center text-center">
+        <Title title="CONTACTO" des="Enviame tu consulta" />
       </div>
-      <div className='w-full'>
-        <div className='w-full h-auto flex flex-col lgl:flex-row justify-between'>
+      <div className="w-full">
+        <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
           <ContactLeft />
-          <div className='w-full lgl:w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne'>
+          <div className="w-full lgl:w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne">
             <form
               ref={form}
               onSubmit={sendEmail}
-              className='w-full flex flex-col gap-4 lgl:gap-6 py-2 lgl:py-5'
+              className="w-full flex flex-col gap-4 lgl:gap-6 py-2 lgl:py-5"
             >
               {errMsg && (
-                <p className='py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce'>
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
                   {errMsg}
                 </p>
               )}
               {successMsg && (
-                <p className='py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce'>
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce">
                   {successMsg}
                 </p>
               )}
-              <div className='w-full flex flex-col lgl:flex-row gap-10'>
-                <div className='w-full lgl:w-1/2 flex flex-col gap-4'>
-                  <p className='text-sm text-gray-400 uppercase tracking-wide'>
+              <div className="w-full flex flex-col lgl:flex-row gap-10">
+                <div className="w-full lgl:w-1/2 flex flex-col gap-4">
+                  <p className="text-sm text-gray-400 uppercase tracking-wide">
                     Apellido y Nombre
                   </p>
                   <input
                     onChange={(e) => setName(e.target.value)}
                     value={name}
                     className={`${
-                      errMsg === 'Apellido y Nombre son requeridos!' &&
-                      'outline-designColor'
+                      errMsg === "Apellido y Nombre son requeridos!" &&
+                      "outline-designColor"
                     } contactInput`}
-                    type='text'
-                    name='name'
+                    type="text"
+                    name="name"
                   />
                 </div>
-                <div className='w-full lgl:w-1/2 flex flex-col gap-4'>
-                  <p className='text-sm text-gray-400 uppercase tracking-wide'>
+                <div className="w-full lgl:w-1/2 flex flex-col gap-4">
+                  <p className="text-sm text-gray-400 uppercase tracking-wide">
                     Teléfono
                   </p>
                   <input
                     onChange={(e) => setPhoneNumber(e.target.value)}
                     value={phoneNumber}
                     className={`${
-                      errMsg === 'Teléfono es requerido!' &&
-                      'outline-designColor'
+                      errMsg === "Teléfono es requerido!" &&
+                      "outline-designColor"
                     } contactInput`}
-                    type='text'
-                    name='phone'
+                    type="text"
+                    name="phone"
                   />
                 </div>
               </div>
-              <div className='flex flex-col gap-4'>
-                <p className='text-sm text-gray-400 uppercase tracking-wide'>
+              <div className="flex flex-col gap-4">
+                <p className="text-sm text-gray-400 uppercase tracking-wide">
                   Email
                 </p>
                 <input
                   onChange={(e) => setEmail(e.target.value)}
                   value={email}
                   className={`${
-                    errMsg === 'Email es requerido!' && 'outline-designColor'
+                    errMsg === "Email es requerido!" && "outline-designColor"
                   } contactInput`}
-                  type='email'
-                  name='email'
+                  type="email"
+                  name="email"
                 />
               </div>
-              <div className='flex flex-col gap-4'>
-                <p className='text-sm text-gray-400 uppercase tracking-wide'>
+              <div className="flex flex-col gap-4">
+                <p className="text-sm text-gray-400 uppercase tracking-wide">
                   Asunto
                 </p>
                 <input
                   onChange={(e) => setSubject(e.target.value)}
                   value={subject}
                   className={`${
-                    errMsg === 'Asunto es requerido!' && 'outline-designColor'
+                    errMsg === "Asunto es requerido!" && "outline-designColor"
                   } contactInput`}
-                  type='text'
-                  name='subject'
+                  type="text"
+                  name="subject"
                 />
               </div>
-              <div className='flex flex-col gap-4'>
-                <p className='text-sm text-gray-400 uppercase tracking-wide'>
+              <div className="flex flex-col gap-4">
+                <p className="text-sm text-gray-400 uppercase tracking-wide">
                   Consulta
                 </p>
                 <textarea
                   onChange={(e) => setMessage(e.target.value)}
                   value={message}
                   className={`${
-                    errMsg === 'Mensaje es requerido!' && 'outline-designColor'
+                    errMsg === "Mensaje es requerido!" && "outline-designColor"
                   } contactTextArea`}
-                  cols='30'
-                  rows='8'
-                  name='message'
+                  cols="30"
+                  rows="8"
+                  name="message"
                 ></textarea>
               </div>
-              <div className='w-full'>
+              <div className="w-full">
                 <button
-                  type='submit'
-                  className='w-full h-12 bg-[#141518] rounded-lg text-base text-gray-400 tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] hover:border-designColor border-transparent'
+                  type="submit"
+                  className="w-full h-12 bg-[#141518] rounded-lg text-base text-gray-400 tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] hover:border-designColor border-transparent"
                 >
                   Enviar Consulta
                 </button>
               </div>
               {errMsg && (
-                <p className='py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce'>
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
                   {errMsg}
                 </p>
               )}
               {successMsg && (
-                <p className='py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce'>
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce">
                   {successMsg}
                 </p>
               )}
--- src/components/resume/Education.jsx
+++ src/components/resume/Education.jsx
@@ -1,5 +1,5 @@
-import { motion } from 'framer-motion';
-import { ResumeCard } from './ResumeCard';
+import { motion } from "framer-motion";
+import { ResumeCard } from "./ResumeCard";
 
 export const Education = () => {
   const fechaActual = new Date();
@@ -9,43 +9,48 @@ export const Education = () => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
-      className='w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20'
+      className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
     >
       {/* part one */}
       <div>
-        <div className='py-6 lgl:py-12 font-titleFont flex flex-col gap-4'>
-          <p className='text-sm text-designColor tracking-[4px]'>
+        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]">
             2015 - {añoActual}
           </p>
-          <h2 className='text-3xl md:text-4xl font-bold'>
+          <h2 className="text-3xl md:text-4xl font-bold">
             Estudios Realizados
           </h2>
         </div>
-        <div className='mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10'>
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title='Analista Programador Universitario'
-            subTitle='Facultad de Informática UNLP (Enero 2015 - Diciembre 2021)'
-            result='7'
-            des='La carrera Analista Programador Universitario tiene como objetivo la formación de un graduado con conocimientos básicos de los fundamentos de la disciplina y de las tecnologías actuales, a fin de resultar capacitado para el trabajo profesional en sistemas de pequeña y mediana complejidad.'
+            title="Analista Programador Universitario"
+            subTitle="Facultad de Informática UNLP (Enero 2015 - Diciembre 2021)"
+            result="7"
+            des="La carrera Analista Programador Universitario tiene como objetivo la formación de un graduado con conocimientos básicos de los fundamentos de la disciplina y de las tecnologías actuales, a fin de resultar capacitado para el trabajo profesional en sistemas de pequeña y mediana complejidad."
           />
           <ResumeCard
-            title='Wordpress'
-            subTitle='Coderhouse (Marzo 2021 - Abril 2021)'
-            result='10'
-            des='Crearás tu propia Tienda Online con pasarelas de pago. Al terminar el curso serás capaz de crear y configurar sitios web totalmente responsive, con e-commerce incluido.'
+            title="Wordpress"
+            subTitle="Coderhouse (Marzo 2021 - Abril 2021)"
+            result="10"
+            des="Crearás tu propia Tienda Online con pasarelas de pago. Al terminar el curso serás capaz de crear y configurar sitios web totalmente responsive, con e-commerce incluido."
           />
           <ResumeCard
-            title='JavaScript Moderno: Guía para dominar el lenguaje'
-            subTitle='Udemy (Marzo 2022 - Marzo 2022)'
-            result=''
-            des='El objetivo principal del curso es enseñarte JavaScript actual, empezando de cero conocimiento en JavaScript hasta llevarte a un nivel avanzado y competitivo en el mercado laboral actual.'
+            title="JavaScript Moderno: Guía para dominar el lenguaje"
+            subTitle="Udemy (Marzo 2022 - Marzo 2022)"
+            result=""
+            des="El objetivo principal del curso es enseñarte JavaScript actual, empezando de cero conocimiento en JavaScript hasta llevarte a un nivel avanzado y competitivo en el mercado laboral actual."
           />
           <ResumeCard
-            title='React Js'
-            subTitle='Coderhouse (Julio 2022 - Agosto 2022)'
-            result='8'
-            des='En este curso aprenderás a programar por componentes, mediante Javascript, JS, ES6, y también conocerás las ventajas de la utilización del flujos de datos.
-            Al finalizar, podrás crear tus propias aplicaciones SPA, y estarás en condiciones de hacer cualquier desarrollo con uno de los librerias más populares y avanzadas del momento.'
+            title="React Js"
+            subTitle="Coderhouse (Julio 2022 - Agosto 2022)"
+            result="8"
+            des="En este curso aprenderás a programar por componentes, mediante Javascript, JS, ES6, y también conocerás las ventajas de la utilización del flujos de datos.
+            Al finalizar, podrás crear tus propias aplicaciones SPA, y estarás en condiciones de hacer cualquier desarrollo con uno de los librerias más populares y avanzadas del momento."
+          />
+          <ResumeCard
+            title="Node: De cero a experto"
+            subTitle="Udemy (Septiembre 2023 - Septiembre 2023)"
+            des="El objetivo de este curso, es enseñar Node con ejercicios reales y aplicables en la vida real, aprender haciendo aplicaciones, incrementando la complejidad de las mismas poco a poco, y sin darte cuenta estaremos trabajando con bases de datos, sockets, consumiendo servicios y mucho más."
           />
         </div>
       </div>

437799a2f82a2ae32ab02c09c9b5f7844248d540
Fermin Perez <fermin_05@hotmail.com>
2023-10-02 11:11:00 -0300

    Edit Data

--- src/data/data.js
+++ src/data/data.js
@@ -1,24 +1,24 @@
-import { AiFillAppstore } from 'react-icons/ai';
-import { FaWindows, FaShoppingCart } from 'react-icons/fa';
+import { AiFillAppstore } from "react-icons/ai";
+import { FaWindows, FaShoppingCart } from "react-icons/fa";
 
 // Features Data
 export const featuresData = [
   {
     id: 1,
     icon: <AiFillAppstore />,
-    title: 'Sitios Web',
-    des: 'Mi objetivo es diseñar y desarrollar sitios web atractivos y funcionales que cautiven a los visitantes y cumplan con los objetivos de mis clientes. Desde sitios web corporativos hasta tiendas en línea y blogs.',
+    title: "Sitios Web",
+    des: "Mi objetivo es diseñar y desarrollar sitios web atractivos y funcionales que cautiven a los visitantes y cumplan con los objetivos de mis clientes.",
   },
   {
     id: 2,
     icon: <FaWindows />,
-    title: 'Aplicaciones Web',
-    des: 'Trabajo en estrecha colaboración con mis clientes para comprender sus necesidades y objetivos comerciales, y luego transformo esas ideas en soluciones web personalizadas.',
+    title: "Aplicaciones Web",
+    des: "Trabajo en estrecha colaboración con mis clientes para comprender sus necesidades y luego transformo esas ideas en soluciones web.",
   },
   {
     id: 3,
     icon: <FaShoppingCart />,
-    title: 'E-Commerce',
-    des: 'Mi enfoque se centra en diseñar y desarrollar plataformas de comercio electrónico atractivas, funcionales y altamente efectivas que impulsen el crecimiento y el éxito de tu negocio.',
+    title: "E-Commerce",
+    des: "Mi enfoque se centra en diseñar y desarrollar plataformas de comercio electrónico atractivas, funcionales y altamente efectivas.",
   },
 ];

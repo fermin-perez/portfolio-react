7069cc6e3e478301a7e9d4eaafde4eb0a1559617
Fermin Perez <101753600+fermin-perez@users.noreply.github.com>
2023-11-10 16:16:41 -0300

    Proyects and Skills updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,17 @@
-import logo from './images/logo.png';
-import bannerImg from './images/bannerImg.jpg';
-import Nurban from './images/projects/nurban.png';
-import Xperiment from './images/projects/xperiment.png';
-import MuniBragado from './images/projects/munibragado.png';
-import HCDBragado from './images/projects/hcdbragado.png';
-import LSARG from './images/projects/ls.jpg';
-import SEF from './images/projects/sef.jpg';
-import testimonialOne from './images/testmonial/testimonialOne.png';
-import testimonialTwo from './images/testmonial/testimonialTwo.png';
-import quote from './images/testmonial/quote.png';
-import contactImg from './images/contact/contactImg.jpg';
+import logo from "./images/logo.png";
+import bannerImg from "./images/bannerImg.jpg";
+import Nurban from "./images/projects/nurban.png";
+import Xperiment from "./images/projects/xperiment.png";
+import MuniBragado from "./images/projects/munibragado.png";
+import HCDBragado from "./images/projects/hcdbragado.png";
+import LSARG from "./images/projects/ls.jpg";
+import JMPBA from "./images/projects/jmpba.jpg";
+import BragadoEsNoticia from "./images/projects/bragado_esnoticia.jpeg";
+import SEF from "./images/projects/sef.jpg";
+import testimonialOne from "./images/testmonial/testimonialOne.png";
+import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+import quote from "./images/testmonial/quote.png";
+import contactImg from "./images/contact/contactImg.jpg";
 
 export {
   logo,
@@ -19,7 +21,9 @@ export {
   MuniBragado,
   HCDBragado,
   LSARG,
+  JMPBA,
   SEF,
+  BragadoEsNoticia,
   testimonialOne,
   testimonialTwo,
   quote,
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,59 +1,74 @@
-import { Title } from '../layouts/Title';
+import { Title } from "../layouts/Title";
 import {
   HCDBragado,
   LSARG,
   MuniBragado,
   Nurban,
-  SEF,
+  JMPBA,
+  BragadoEsNoticia,
   Xperiment,
-} from '../../assets/index';
-import { ProjectsCard } from './ProjectsCard';
+  SEF,
+} from "../../assets/index";
+import { ProjectsCard } from "./ProjectsCard";
 
 export const Projects = () => {
   return (
     <section
-      id='proyectos'
-      className='w-full py-20 border-b-[1px] border-b-black'
+      id="proyectos"
+      className="w-full py-20 border-b-[1px] border-b-black"
     >
-      <div className='flex justify-center items-center text-center'>
-        <Title title='Proyectos' des='Algunos de mis proyectos más recientes' />
+      <div className="flex justify-center items-center text-center">
+        <Title title="Proyectos" des="Algunos de mis proyectos más recientes" />
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14'>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
-          title='Municipalidad de Bragado'
-          des='Desarrollo del sitio web de la Municipalidad de Bragado.'
+          title="Municipalidad de Bragado"
+          des="Desarrollo del sitio web de la Municipalidad de Bragado."
           src={MuniBragado}
-          link='https://www.bragado.gov.ar/'
+          link="https://www.bragado.gov.ar/"
         />
         <ProjectsCard
-          title='HCD Bragado'
-          des='Desarrollo del sitio web del HCD de Bragado.'
+          title="HCD Bragado"
+          des="Desarrollo del sitio web del HCD de Bragado."
           src={HCDBragado}
-          link='https://hcdbragado.gob.ar/'
+          link="https://hcdbragado.gob.ar/"
         />
         <ProjectsCard
-          title='LS ARG'
-          des='Desarrollo del sitio web de LS Electromecánica.'
+          title="LS ARG"
+          des="Desarrollo del sitio web de LS Electromecánica."
           src={LSARG}
-          link='https://ls-arg.com/'
+          link="https://ls-arg.com/"
         />
         <ProjectsCard
-          title='Xperiment VC'
-          des='Desarrollo del sitio web de Xperiment Startup Studio & VC'
+          title="Xperiment VC"
+          des="Desarrollo del sitio web de Xperiment Startup Studio & VC."
           src={Xperiment}
-          link='https://www.xperiment.co/'
+          link="https://www.xperiment.co/"
         />
         <ProjectsCard
-          title='NURBAN'
-          des='Desarrollo del sitio web de Nurban Desarrollos Inmobiliarios.'
+          title="NURBAN"
+          des="Desarrollo del sitio web de Nurban Desarrollos Inmobiliarios."
           src={Nurban}
-          link='https://nurban.com.ar/'
+          link="https://nurban.com.ar/"
         />
         <ProjectsCard
-          title='SEF'
-          des='Desarrollo del sistema de administración del Sindicato de Empleados de Farmacia.'
+          title="Justicia Municipal PBA"
+          des="Desarrollo del sitio web de la Asociación de la
+          Justicia Municipal Bonaerense."
+          src={JMPBA}
+          link="https://justiciamunicipalpba.org/"
+        />
+        <ProjectsCard
+          title="Bragado Es Noticia"
+          des="Desarrollo del sitio web de Bragado Es Noticia."
+          src={BragadoEsNoticia}
+          link="https://bragadoesnoticia.com.ar/"
+        />
+        <ProjectsCard
+          title="SEF"
+          des="Desarrollo del sistema de administración del Sindicato de Empleados de Farmacias."
           src={SEF}
-          link='https://sistemassef.com.ar/public/'
+          link="#"
         />
       </div>
     </section>
